273878ac3e7dde0541d09029fa35da8a5cd84e5b
Matthew Scarth <mscarth1@gmail.com>
2023-04-17 19:03:34 +0100

    add grid and change colour of individual squares on mouse over

--- etch.js
+++ etch.js
@@ -1,8 +1,28 @@
 
 const container = document.querySelector('#container');
 
-const grid = document.createElement('grid');
-grid.classList.add('grid');
+let grid = document.createElement('div');
+grid.className = 'grid';
+
+    let columns = 16;
+    let rows = 16;
+
+    for (i = 0; i < columns; ++i) {
+        let column = document.createElement('div');
+        column.className = 'column';
+        grid.appendChild(column);
+        
+        for (j = 0; j < rows; ++j) {
+            let row = document.createElement('div');
+            row.className = 'row';
+            column.appendChild(row);
+        }
+        
+    }
+    
+    container.appendChild(grid)
 
 
-container.appendChild(grid);
+    grid.addEventListener("mouseover", function( event ) {   
+        event.target.style.backgroundColor = "blue";
+    })
