d728c031975bbf3982cd3554cd6018373a028285
Matthew Scarth <mscarth1@gmail.com>
2023-04-18 17:16:23 +0100

    add ability to create new grid with any size

--- etch.js
+++ etch.js
@@ -1,28 +1,41 @@
 
 const container = document.querySelector('#container');
 
+
 let grid = document.createElement('div');
 grid.className = 'grid';
 
-    let columns = 16;
-    let rows = 16;
+grid.addEventListener("mouseover", function( event ) {   
+    event.target.style.backgroundColor = "blue";
+})
+    
 
-    for (i = 0; i < columns; ++i) {
+   
+function makeGrid (x) {
+    grid.replaceChildren();
+    
+    for (i = 0; i < x; ++i) {
         let column = document.createElement('div');
         column.className = 'column';
         grid.appendChild(column);
         
-        for (j = 0; j < rows; ++j) {
+        for (j = 0; j < x; ++j) {
             let row = document.createElement('div');
             row.className = 'row';
             column.appendChild(row);
         }
         
     }
-    
     container.appendChild(grid)
+}
 
 
-    grid.addEventListener("mouseover", function( event ) {   
-        event.target.style.backgroundColor = "blue";
-    })
+
+function newGrid() {
+    let x = prompt("Enter a number below 100:");
+    makeGrid(x);
+}
+
+
+
+
